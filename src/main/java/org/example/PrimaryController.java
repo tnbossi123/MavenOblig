14af0db5e27a9b6e4d34884d4eeae58cc4749975
Ardian <ardian1811@gmail.com>
2020-04-30 23:10:32 +0200

    fxml commit onaction virker ikke

--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -1,12 +1,20 @@
 package org.example;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private Button primaryButton;
+
+    @FXML
+    void switchToSecondary(ActionEvent event) {
+
     }
+
 }
