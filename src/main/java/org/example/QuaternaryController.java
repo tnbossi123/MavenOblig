4f0a699856cdb60e037dd2e1a47714b4f71dfd3b
Blerton <tnbossi@me.com>
2020-05-14 01:56:11 +0200

    added back button to the fourth controller

--- src/main/java/org/example/QuaternaryController.java
+++ src/main/java/org/example/QuaternaryController.java
@@ -1,6 +1,16 @@
 package org.example;
 
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class QuaternaryController {
+
+    @FXML
+    void switchToPrimary(ActionEvent event) throws IOException {
+    App.setRoot("primary");
+    }
 
 }
